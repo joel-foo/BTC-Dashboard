26e3266e8ddca56ee728dbc131a5c235b7525263
strawberryminion <joelfootaimeng@gmail.com>
2022-05-13 15:48:24 +0800

    added bash script

---
+++ core_start.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+echo "Checking for Bitcoin Core..."
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  DIR="$HOME/.bitcoin/"
+  if [ ! -d "$DIR" ]; then
+    echo "You do not have Bitcoin Core installed. Please install it at ......"
+    exit 1
+  fi
+  
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  DIR="$HOME/Library/Application Support/Bitcoin/"
+  if [ ! -d "$DIR" ]; then
+    echo "You do not have Bitcoin Core installed. Please install it at ......"
+    exit 1
+  fi
+  
+elif [[ "$OSTYPE" == 'msys'* ]]; then
+  DIR="$HOME/.bitcoin/"
+  if [ ! -d "$DIR" ]; then
+    echo "You do not have Bitcoin Core installed. Please install it at ......"
+    exit 1
+  fi
+  
+else
+  echo "$OSTYPE is not supported"
+  exit 1
+  
+fi
+
+
+echo "You have bitcoin core installed!"
+echo "Starting bitcoin core now........."
+bitcoind -server -daemon -rpcuser=minion -rpcpassword=minion 
+echo "Waiting for bitcoin node....This process can take a few minutes...."
+until bitcoin-cli getblockchaininfo 2>/dev/null; do
+  sleep 1
+done
+echo "Serving up your server and dashboard now..."
+node server/index.js & cd client && npm start &
+    
+  
