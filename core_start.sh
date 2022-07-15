cdcc01ee994e4950b5c4866eff5bb4ecec057697
joel-foo <joelfootaimeng@gmail.com>
2022-07-15 16:40:23 +0800

    edited

--- core_start.sh
+++ core_start.sh
@@ -2,33 +2,31 @@
 
 echo "Checking for Bitcoin Core..."
 
-displayMessage() {
-  echo "You do not have Bitcoin Core installed. Please install it at https://bitcoin.org/en/download"
-  exit 1
+check(){
+  if [ ! -d $1 ]; then
+    echo "You do not have Bitcoin Core installed. Please install it at https://bitcoin.org/en/download. Ensure you have bitcoind added to your path."
+    exit 1
+  fi 
+  which bitcoind
+  if [$? != 0 ]; then
+    echo "You have Bitcoin Core installed but have not added bitcoind to your path variables."
+    exit 1
 }
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   DIR="$HOME/.bitcoin/"
-  if [ ! -d "$DIR" ]; then
-    displayMessage
-  fi
   
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   DIR="$HOME/Library/Application Support/Bitcoin/"
-  if [ ! -d "$DIR" ]; then
-    displayMessage
-  fi
-  
+
 elif [[ "$OSTYPE" == 'msys'* ]]; then
   DIR="$APPDATA/Bitcoin/"
-  if [ ! -d "$DIR" ]; then
-    displayMessage
-  fi
   
 else
   echo "$OSTYPE is not supported"
   exit 1
-  
+
+check "$DIR"
 fi
 
 # Check if bitcoin core is already running. Stop it and re-run with server and daemon. 
@@ -38,8 +36,9 @@ echo "Starting bitcoin core now........."
 blockchainInfo=$(bitcoin-cli getblockchaininfo) 2>/dev/null
 
 if [[ $blockchainInfo != *'Could not connect'* ]]; then
+  echo "Stopping current instance of Bitcoin Core and initating reconnection in 10 seconds..."
   bitcoin-cli stop 
-  sleep 5
+  sleep 10
 fi
 
 bitcoind -server -daemon
@@ -49,7 +48,7 @@ echo "Waiting for bitcoin node....This process can take a few minutes...."
 until $blockchainInfo; do
   # Check if rpc credentials are configured correctly
   if [[ $blockchaininfo == *'Could not locate RPC credentials'* ]]; then
-    echo "Please configure your rpcuser and rpcpassword credentials in bitcoin.conf first!"
+    echo "Please configure your rpcuser and rpcpassword credentials in bitcoin.conf first."
     exit 1
   fi
   sleep 1
@@ -61,18 +60,20 @@ if [[ $blockchainInfo == *'"pruned":true'* ]]; then
 fi
 
 echo "Building your dashboard now..."
+
 # Build server
-npm i 2>/dev/null && npm run build 2>/dev/null && npm run start 2>/dev/null & 
+(npm i; npm run build; npm run start) & >/dev/null 2>&1
 
 # Build client
-until cd client && npm i 2>/dev/null && npm run build 2>/dev/null && serve -s build; do
-  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    xdg-open http://localhost:3000/
+(cd client; npm i; npm run build; serve -s build; xdg-open http://localhost:3000/) &>/dev/null 2>&1; 
+# do
+#   if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+#     xdg-open http://localhost:3000/
 
-  elif [[ "$OSTYPE" == "darwin"* ]]; then
-    open http://localhost:3000/
+  # elif [[ "$OSTYPE" == "darwin"* ]]; then
+  #   open http://localhost:3000/
 
-  elif [[ "$OSTYPE" == "msys"* ]]; then
-    start http://localhost:3000/
-  fi
-  done &
+  # elif [[ "$OSTYPE" == "msys"* ]]; then
+  #   start http://localhost:3000/
+  # fi
+  # done &
