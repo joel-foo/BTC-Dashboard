2f09feb9b6174be0a0208772278825f488ef37c7
joel-foo <joelfootaimeng@gmail.com>
2022-07-15 18:12:27 +0800

    edited

--- core_start.sh
+++ core_start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-kill -9 $(lsof -i:3000 -t)
 kill -9 $(lsof -i:3001 -t)
+kill -9 $(lsof -i:8000 -t)
 
 echo "Checking for Bitcoin Core..."
 
@@ -8,7 +8,7 @@ check(){
   if [ ! -d $1 ]; then
     echo "You do not have Bitcoin Core installed. Please install it at https://bitcoin.org/en/download. Ensure you have bitcoind added to your path."
     exit 1
-  fi 
+  fi
   which bitcoind
   if [$? != 0 ]; then
     echo "You have Bitcoin Core installed but have not added bitcoind to your path variables."
@@ -18,13 +18,13 @@ check(){
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   DIR="$HOME/.bitcoin/"
-  
+ 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   DIR="$HOME/Library/Application Support/Bitcoin/"
 
 elif [[ "$OSTYPE" == 'msys'* ]]; then
   DIR="$APPDATA/Bitcoin/"
-  
+ 
 else
   echo "$OSTYPE is not supported"
   exit 1
@@ -32,16 +32,15 @@ else
 check "$DIR"
 fi
 
-# Check if bitcoin core is already running. Stop it and re-run with server and daemon. 
+# Check if bitcoin core is already running. Stop it and re-run with server and daemon.
 
 echo "Starting bitcoin core now........."
 
-blockchainInfo=$(bitcoin-cli getblockchaininfo) &>/dev/null 2>&1
+blockchainInfo=$(bitcoin-cli getblockchaininfo) >/dev/null 2>&1
 
 if [[ $blockchainInfo != *'Could not connect'* ]]; then
   echo "Stopping current instance of Bitcoin Core and initating reconnection in 10 seconds..."
-  bitcoin-cli stop >/dev/null 2>&1
-  sleep 10
+  sleep 15
 fi
 
 bitcoind -server -daemon
@@ -67,18 +66,7 @@ fi
 echo "Building your dashboard now..."
 
 # Build server
-(npm i; npm run build; npm run start) &>/dev/null 2>&1
+(npm i; npm run build; npm run start)&
 
 # Build client
-(cd client; npm i; npm run build; serve -s build) &>/dev/null 2>&1 && { echo "Completed!"; xdg-open http://localhost:3000/}
-# do
-#   if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-#     xdg-open http://localhost:3000/
-
-  # elif [[ "$OSTYPE" == "darwin"* ]]; then
-  #   open http://localhost:3000/
-
-  # elif [[ "$OSTYPE" == "msys"* ]]; then
-  #   start http://localhost:3000/
-  # fi
-  # done &
+(cd client; npm i; npm run build; serve -s build -p 8000)&
