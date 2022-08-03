b0c2d81f1ebbbdd5d00211bde04e031c905bda56
joel-foo <joelfootaimeng@gmail.com>
2022-08-04 00:40:19 +0800

    added tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // mode: 'jit',
-  // purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     screens: {
@@ -9,9 +7,6 @@ module.exports = {
       md: '768px',
       lg: '976px',
       xl: '1440px',
-    },
-    height: {
-      home: '800px',
     },
     extend: {},
   },
