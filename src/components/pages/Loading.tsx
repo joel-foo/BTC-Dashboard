8d396cb6c2ae4580b57d21cf03cbf1500670dfb1
joel-foo <joelfootaimeng@gmail.com>
2022-08-04 21:58:27 +0800

    edited

--- src/components/pages/Loading.tsx
+++ src/components/pages/Loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className='container mx-auto flex flex-col items-center gap-y-6 py-20'>
+    <div className='container mx-auto flex flex-col items-center gap-y-6 py-10 md:py-20'>
       <h1 className='text-2xl'>Processing blocks...</h1>
       <div className='loader'></div>
     </div>
